90251200b5e85eee40175c28a86eed0ab375c051
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-03-06 09:04:51 +0200

    pet store initial

---
+++ PetStore/PetStore.Common/DbConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace PetStore.Common
+{
+    public static class DbConfiguration
+    {
+        public static string DefConnectionString = @"Server=.\SQLEXPRESS;Database=PetStore;Integrated Security=True;";
+    }
+}
