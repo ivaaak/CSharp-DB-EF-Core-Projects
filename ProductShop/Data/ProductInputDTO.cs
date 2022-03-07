5078ac6bc6f70664633b91027a4c362f7c522835
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-03-07 09:57:50 +0200

    added data - input and output dtos, context

---
+++ ProductShop/Data/ProductInputDTO.cs
@@ -0,0 +1,17 @@
+﻿//ProductInputDTO
+public class ProductInputDTO
+{
+	
+	public string Name { get; set; }
+	
+	public decimal Price { get; set; }
+	
+	public int SellerId { get; set; }
+	
+	public int? BuyerId { get; set; } 	
+}
+//JSON Template:
+//"Name": "PAMO Kill Natural",
+//"Price": 1181.06,
+//"SellerId": 52,
+//"BuyerId": null
---
+++ ProductShop/Data/UserOutputDto.cs
@@ -0,0 +1,9 @@
+﻿////UserOutputDto 
+public class UserOutputDto
+{
+	public string Name { get; set; }
+		 
+	public decimal Price { get; set; }
+		 
+	public string Seller { get; set; }
+}
