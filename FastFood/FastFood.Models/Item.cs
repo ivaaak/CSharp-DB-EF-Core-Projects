f840cede7241677e7abd2296d7752a67a8d72129
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-03-19 18:23:33 +0200

    fixes

--- FastFood/FastFood.Models/Item.cs
+++ FastFood/FastFood.Models/Item.cs
@@ -1,23 +1,23 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace FastFood.Models
 {
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
     public class Item
-	{
-		public int Id { get; set; }
+    {
+	public int Id { get; set; }
 
-		[StringLength(30, MinimumLength = 3)]
-		public string Name { get; set; }
+	[StringLength(30, MinimumLength = 3)]
+	public string Name { get; set; }
 
-		public int CategoryId { get; set; }
+	public int CategoryId { get; set; }
 
-		[Required]
-		public Category Category { get; set; }
+	[Required]
+	public Category Category { get; set; }
 
-		[Range(typeof(decimal), "0.01", "10000")]
-		public decimal Price { get; set; }
+	[Range(typeof(decimal), "0.01", "10000")]
+	public decimal Price { get; set; }
 
-		public ICollection<OrderItem> OrderItems { get; set; }
-	}
+	public ICollection<OrderItem> OrderItems { get; set; }
+    }
 }
