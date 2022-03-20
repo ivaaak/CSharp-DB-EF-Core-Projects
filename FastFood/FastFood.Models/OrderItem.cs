4bcbbd2b033967efe0938a93ef2df456f850a934
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-03-20 10:13:46 +0200

    Update OrderItem.cs

--- FastFood/FastFood.Models/OrderItem.cs
+++ FastFood/FastFood.Models/OrderItem.cs
@@ -1,20 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FastFood.Models
 {
-    using System.ComponentModel.DataAnnotations;
-
     public class OrderItem
-	{
-		public int OrderId { get; set; }
+    {
+	public int OrderId { get; set; }
 
-		[Required]
-		public Order Order { get; set; }
+	[Required]
+	public Order Order { get; set; }
 
-		public int ItemId { get; set; }
+	public int ItemId { get; set; }
 
-		[Required]
-		public Item Item { get; set; }
+	[Required]
+	public Item Item { get; set; }
 
-		[Range(1, int.MaxValue)]
-		public int Quantity { get; set; }
-	}
+	[Range(1, int.MaxValue)]
+	public int Quantity { get; set; }
+    }
 }
