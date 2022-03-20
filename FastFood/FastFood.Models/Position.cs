34cb970596f035cf4f3c2f9af460e8c7cd9829f4
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-03-20 10:14:51 +0200

    Update Position.cs

--- FastFood/FastFood.Models/Position.cs
+++ FastFood/FastFood.Models/Position.cs
@@ -1,16 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace FastFood.Models
 {
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
     public class Position
-	{
-		public int Id { get; set; }
+    {
+	public int Id { get; set; }
 
-		[Required]
-		[StringLength(30, MinimumLength = 3)]
-		public string Name { get; set; }
+	[Required]
+	[StringLength(30, MinimumLength = 3)]
+	public string Name { get; set; }
 
-		public ICollection<Employee> Employees { get; set; }
-	}
+	public ICollection<Employee> Employees { get; set; }
+    }
 }
