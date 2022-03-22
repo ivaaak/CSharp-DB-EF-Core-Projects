38b29096e4e582422bc042325ea9d9dd50cf792d
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-03-22 22:34:31 +0200

    Update Startup.cs

--- MyQuiz/MyQuizApplication/Startup.cs
+++ MyQuiz/MyQuizApplication/Startup.cs
@@ -32,11 +32,10 @@ namespace MyQuizApplication
                 .UseSnakeCaseNamingConvention());
             services.AddScoped<IQuizRepository, QuizRepository>();
             services.AddScoped<IQuizService, QuizService>();
-            //options.UseSqlServer(Configuration.GetConnectionString("MSSQLConnection")));
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -46,7 +45,6 @@ namespace MyQuizApplication
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
