0c06d55da2b72969cba946e266b1992345ff2de9
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-03-22 22:35:25 +0200

    Update QuizContext.cs

--- MyQuiz/MyQuizApplication.Integrations/Data/QuizContext.cs
+++ MyQuiz/MyQuizApplication.Integrations/Data/QuizContext.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MyQuizApplication.Infrastructure.Data.Models;
-
-// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
-// If you have enabled NRTs for your project, then un-comment the following line:
-// #nullable disable
 
 namespace MyQuizApplication.Infrastructure.Data
 {
