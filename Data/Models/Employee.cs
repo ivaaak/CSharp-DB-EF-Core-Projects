e531b043f73e4d8c3391bf3d05c9cc8ab08be5f2
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-03-23 10:55:48 +0200

    added db models

---
+++ Data/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace TeisterMask.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using Common;
+
+    public class Employee
+    {
+        public Employee()
+        {
+            this.EmployeesTasks = new HashSet<EmployeeTask>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(GlobalConstants.EMPLOYEE_USERNAME_MAX_LENGTH)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(GlobalConstants.EMPLOYEE_PHONE_MAX_LENGTH)]
+        public string Phone { get; set; }
+
+        public virtual ICollection<EmployeeTask> EmployeesTasks { get; set; }
+    }
+}
